dcaea7bb3e6f2e2780b20ed8f4a8917a75d82e9b
seanpierce <sumler.sean@gmail.com>
2026-08-04 20:02:58 -0700

    Render 'about' content in side bar nav

--- friendship/views.py
+++ friendship/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from content.models import About
 
 
 def index(request):
-    return render(request, 'index.html')
+    about = About.objects.first()  # Get the first About object from the database
+    context = {
+        'about': about.info
+    }
+    return render(request, 'index.html', context)
